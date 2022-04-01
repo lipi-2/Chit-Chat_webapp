beeae82017b62807adca056f06a1b71769c07cb0
lipi-2 <97337501+lipi-2@users.noreply.github.com>
2022-04-01 10:42:10 +0530

    Add files via upload

---
+++ chit-chat_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBhysqQR-L2txc-7588btuudBWHwPYPOkk",
+    authDomain: "project-kwitter-f9b4d.firebaseapp.com",
+    projectId: "project-kwitter-f9b4d",
+    storageBucket: "project-kwitter-f9b4d.appspot.com",
+    messagingSenderId: "621131845755",
+    appId: "1:621131845755:web:9a66530998e239181a92de"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
